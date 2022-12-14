aa7377947560fa96327265b17e65b439b5d6da3a
TruptiRG <trupti16ghadekar@gmail.com>
2022-12-14 14:26:31 +0530

    intial commit

---
+++ BusReservationPortal/src/main/java/com/bus/implimentation/ReservationServiceIMPL.java
@@ -0,0 +1,21 @@
+package com.bus.implimentation;
+
+import com.bus.exception.ReservationException;
+import com.bus.model.Reservation;
+import com.bus.service.ReservationService;
+
+public class ReservationServiceIMPL implements ReservationService{
+
+	@Override
+	public Reservation addReservation(Reservation reservation) throws ReservationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation uudateReservation(Reservation reservation) throws ReservationException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
