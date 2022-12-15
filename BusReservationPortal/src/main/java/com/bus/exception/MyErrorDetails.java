951597363f3b95c9040caa104e08c4c5d7ea27e5
TruptiRG <trupti16ghadekar@gmail.com>
2022-12-15 12:36:36 +0530

    rervation

--- BusReservationPortal/src/main/java/com/bus/exception/MyErrorDetails.java
+++ BusReservationPortal/src/main/java/com/bus/exception/MyErrorDetails.java
@@ -4,9 +4,13 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
+@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class MyErrorDetails {
--- BusReservationPortal/src/main/java/com/bus/model/Reservation.java
+++ BusReservationPortal/src/main/java/com/bus/model/Reservation.java
@@ -1,5 +1,13 @@
 package com.bus.model;
 
+import java.time.LocalDate;
+
 public class Reservation {
+	
+	private Integer resevationId;
+	private String reservationStatus;
+	private String reservationType;
+	private LocalDate reservationDate;
+	
 
 }
