10789bcd9f7ae4b925cfe39e49aa78308ace19e3
Pankaj Rai <pankajrai3295@gmail.com>
2022-12-15 13:21:07 +0530

    Day3

--- BusReservationPortal/src/main/java/com/bus/model/Route.java
+++ BusReservationPortal/src/main/java/com/bus/model/Route.java
@@ -1,5 +1,14 @@
 package com.bus.model;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Route {
 
+	
+	public static void main(String[] args) {
+		int x =353;
+		System.out.print(false);
+	}
+	
+	
 }
