a22ad5e25a2bcd59d748a0b7acd3a1e29d90c96c
Pankaj Rai <pankajrai3295@gmail.com>
2022-12-15 14:29:32 +0530

    DAy3 beanClass

--- BusReservationPortal/src/main/java/com/bus/model/Route.java
+++ BusReservationPortal/src/main/java/com/bus/model/Route.java
@@ -1,14 +1,35 @@
 package com.bus.model;
 
-import javax.swing.plaf.synth.SynthOptionPaneUI;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Route {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer routeId;
 	
-	public static void main(String[] args) {
-		int x =353;
-		System.out.print(false);
-	}
+	private String routeFrom;
+	
+	private String routeTo;
+	
+	private Integer distance;
+	
+	
 	
 	
 }
