518c47c1c1906c6dfc30012366a30d96f6d8cfc8
Ashish898374 <yadavashish347347@gmail.com>
2022-12-15 14:26:41 +0530

    fp04_100feedback

--- BusReservationPortal/src/main/java/com/bus/model/Feedback.java
+++ BusReservationPortal/src/main/java/com/bus/model/Feedback.java
@@ -1,5 +1,32 @@
 package com.bus.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Feedback {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Integer feedbackid;
+    private Integer driverRating;
+    private Integer ServiceRating;
+    private Integer OverallRating;
+    private String comment;
+    private LocalDateTime feedbackdate;
+//    private  User user;
+//    private  Bus bus;
+
 
 }
