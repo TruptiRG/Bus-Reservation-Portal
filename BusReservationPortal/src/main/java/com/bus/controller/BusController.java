68915ee389e8f9829b1d82fb01c663536c113615
Shubham Patil <shubhamppatil1996.com>
2022-12-15 20:32:50 +0530

    fp04_139_day-3

---
+++ BusReservationPortal/src/main/java/com/bus/controller/BusController.java
@@ -0,0 +1,66 @@
+package com.bus.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bus.exception.BusException;
+import com.bus.model.Bus;
+import com.bus.repository.BusRepo;
+import com.bus.service.BusService;
+
+@RestController
+@RequestMapping("/busservice")
+public class BusController {
+	
+	@Autowired
+	private BusService busservice;
+	
+	@PostMapping("/addbus")
+	public ResponseEntity<Bus> AddBusesHandler(@RequestBody Bus bus) throws BusException{
+		Bus addnewbus = busservice.addBus(bus);
+		return new ResponseEntity<Bus>(addnewbus, HttpStatus.CREATED);
+		
+	}
+	@PutMapping("/updatedbus")
+	public ResponseEntity<Bus> UpdateBusDetailsHandler(@RequestBody Bus bus) throws BusException{
+		
+		Bus updatedBusDetails=busservice.updateBus(bus); 
+		
+		return new ResponseEntity<Bus>(updatedBusDetails,HttpStatus.ACCEPTED);	
+	}
+	@DeleteMapping("/deletebustbyid/{busId}")
+	public ResponseEntity<Bus> DeleteBusByIdHandler(Integer busId)throws BusException{
+		return new ResponseEntity<Bus>(busservice.deleteBus(busId),HttpStatus.FOUND);
+		
+	}
+	
+	@GetMapping("/getbybusid/{busId}")
+	public ResponseEntity<Bus> GetBusByidHandler(@PathVariable Integer busId) throws BusException{
+		return new ResponseEntity<Bus>(busservice.viewBus(busId),HttpStatus.FOUND);
+	}
+	
+	@GetMapping("/getallbusesbytype/{busType}")
+	public ResponseEntity<List<Bus>> GetBusDetailsBybusType(@PathVariable("busType") String busType)throws BusException{
+		List<Bus> list = busservice.viewBusByType(busType);
+		return new ResponseEntity<List<Bus>>(list,HttpStatus.FOUND);
+	}
+
+	@GetMapping("/getallbuslist")
+	public ResponseEntity<List<Bus>>GetAllBusDetails()throws BusException{
+		return new ResponseEntity<List<Bus>>(busservice.viewAllBus(),HttpStatus.FOUND);
+		
+	}
+	
+	
+}
--- BusReservationPortal/src/main/java/com/bus/implimentation/BusServiceImpl.java
+++ BusReservationPortal/src/main/java/com/bus/implimentation/BusServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bus.implimentation;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,33 +19,67 @@ public class BusServiceImpl implements BusService{
 
 	@Override
 	public Bus addBus(Bus bus) throws BusException {
-		return null;
 		
+		Bus addbus = busrepo.save(bus);
+		
+		if(bus!=null) {
+			return bus;
+		}else {
+			throw new BusException("Bus Details Not Added");
+		}	
 	}
 
 	@Override
 	public Bus updateBus(Bus bus) throws BusException {
-		return null;
+		Optional<Bus> opt = busrepo.findById(bus.getBusId());
+		if(opt.isPresent()) {
+			Bus updateBus = busrepo.save(bus);
+			
+			return updateBus;
+		} else {
+			throw new BusException("Invalid Bus Details");
+		}
 	}
 
 	@Override
 	public Bus deleteBus(Integer busId) throws BusException {
-		return null;
+       Optional<Bus> opt = busrepo.findById(busId);
+		
+		if(opt.isPresent()) {
+			Bus bus = opt.get();
+			
+			busrepo.delete(bus);
+			return bus;
+		} else {
+			throw new BusException("Bus Not Found :"+busId);
+		}
 	}
 
 	@Override
 	public Bus viewBus(Integer busId) throws BusException {
-		return null;
+		return busrepo.findById(busId).orElseThrow(()-> new BusException());
 	}
 
 	@Override
 	public List<Bus> viewBusByType(String busType) throws BusException {
-		return null;
+		List<Bus> list = busrepo.viewBusByType(busType);
+		if(list.size()>0) {
+			return list;
+		}
+		throw new BusException("Bus Not Found With Given Type :"+busType);
+		
 	}
 
 	@Override
 	public List<Bus> viewAllBus() throws BusException {
-		return null;
+         List<Bus> bus = busrepo.findAll();
+		
+		if(bus.size()>0) {
+			return bus;
+		} else {
+			
+			throw new BusException("No Bus Found");
+		}
 	}
 
 }
